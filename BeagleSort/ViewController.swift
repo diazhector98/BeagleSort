aa1cf83005cde8a8f4469119da1a9f13155af196
Hector Diaz <diazhector98@gmai.com>
2019-10-07 10:08:36 -0500

    Initial Commit

---
+++ BeagleSort/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeagleSort
+//
+//  Created by Hector Díaz Aceves on 07/10/19.
+//  Copyright © 2019 WichoInc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
