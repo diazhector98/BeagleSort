6aa0b79296cac6c5dee5bae8307b7023ad70e854
Hector Diaz <diazhector98@gmai.com>
2019-10-11 23:53:32 -0500

    Creating DemonstrationViewController

---
+++ BeagleSort/DemonstrationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DemonstrationViewController.swift
+//  BeagleSort
+//
+//  Created by Hector Díaz Aceves on 11/10/19.
+//  Copyright © 2019 WichoInc. All rights reserved.
+//
+
+import UIKit
+
+class DemonstrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
