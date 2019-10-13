213b66427ace4c8f3a262d93d273ddffaa664bcf
Pedro Villezca <pedrovillezca@Pedros-MacBook-Pro.local>
2019-10-13 12:07:52 -0500

    Carga los datos del algoritmo en la pagina de informacion

--- BeagleSort/AlgorithmViewController.swift
+++ BeagleSort/AlgorithmViewController.swift
@@ -9,11 +9,19 @@
 import UIKit
 
 class AlgorithmViewController: UIViewController {
-
+    @IBOutlet weak var lbPromedio: UILabel!
+    @IBOutlet weak var lbPeor: UILabel!
+    @IBOutlet weak var lbMemoria: UILabel!
+    @IBOutlet weak var lbCodigo: UILabel!
+    
+    var algorithm : Algorithm!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        lbPromedio.text = algorithm.averageCase
+        lbPeor.text = algorithm.worstCase
+        lbMemoria.text = algorithm.spaceComplexity
+        lbCodigo.text = algorithm.implementation
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,7 +36,7 @@ class AlgorithmViewController: UIViewController {
         
         if segue.identifier == "demostracion" {
             let vista = segue.destination as! DemonstrationViewController
-            vista.algorithm = BubbleSort.instance
+            vista.algorithm = algorithm
             vista.array = [3,4,2,5,2,8,7]
         }
         
