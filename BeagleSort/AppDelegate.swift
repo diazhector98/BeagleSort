b6d189cb0bb6e3316ebddf651a72e4fad94d38bf
Hector Diaz <diazhector98@gmai.com>
2019-11-13 11:51:00 -0600

    Create QuickSort algorithm class

--- BeagleSort/AppDelegate.swift
+++ BeagleSort/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         BubbleSort.create()
         MergeSort.create()
         InsertionSort.create()
+        QuickSort.create()
         return true
     }
 
