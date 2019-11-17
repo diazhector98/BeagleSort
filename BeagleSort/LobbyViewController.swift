3fdec463332e56e912ac5a9392e8ced08caf0e85
Cesar Barraza <cbarrazaa1@gmail.com>
2019-11-17 16:40:58 -0600

    lobby now connects to the server

--- BeagleSort/LobbyViewController.swift
+++ BeagleSort/LobbyViewController.swift
@@ -7,17 +7,29 @@
 //
 
 import UIKit
+import Firebase
+import SocketIO
 
 class LobbyViewController: UIViewController {
     @IBOutlet weak var lblStatus: UILabel!
     @IBOutlet weak var btnSearch: UIButton!
     @IBOutlet weak var btnLeaderboard: UIButton!
     
+    let manager = SocketManager(socketURL: URL(string: "http://localhost:3000")!, config: [.log(true), .compress]);
+    
+    private var socket: SocketIOClient!;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         StylesHelper.addButtonStyles(button: self.btnSearch);
         StylesHelper.addButtonStyles(button: self.btnLeaderboard);
+        self.btnSearch.setTitleColor(UIColor(named: "Disabled"), for: .disabled);
+        
+        // init socket
+        self.socket = manager.defaultSocket;
+        createSocketHandlers();
+        self.socket.connect();
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -28,15 +40,59 @@ class LobbyViewController: UIViewController {
         self.navigationController!.setNavigationBarHidden(false, animated: true);
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func showAlert(_ message: String) -> Void {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert);
+        let ok = UIAlertAction(title: "OK", style: .default, handler: nil);
+        alert.addAction(ok);
+        present(alert, animated: false);
     }
-    */
+    
+    private func createSocketHandlers() {
+        self.socket.on(clientEvent: .connect) { (data, ack) in
+            self.lblStatus.text = "Conectado al servidor.";
+            self.lblStatus.textColor = UIColor(red: 39.0/255, green: 161.0/255, blue: 59.0/255, alpha: 1);
+            self.btnSearch.isEnabled = true;
+        }
+        
+        self.socket.on(clientEvent: .error) { (data, ack) in
+            self.lblStatus.text = "No se pudo conectar al servidor.";
+            self.lblStatus.textColor = .red;
+            self.btnSearch.setTitle("Buscar partida", for: .disabled);
+            self.btnSearch.isEnabled = false;
+        }
+        
+        self.socket.on(clientEvent: .reconnectAttempt) { (data, ack) in
+            self.lblStatus.text = "Conectando con el servidor...";
+            self.lblStatus.textColor = .black;
+        }
+        
+        self.socket.on("waiting_queue") { (data, ack) in
+            self.btnSearch.setTitle("Buscando otros jugadores...", for: .disabled);
+            self.btnSearch.isEnabled = false;
+        }
+        
+        self.socket.on("entered_game") { (data, ack) in
+            let dict = (data[0] as? NSDictionary)!;
+            let otherPlayer = dict["otherPlayerName"] as! String;
+            let algorithm = dict["algorithm"] as! String;
+            let arr = (dict["arr"] as! NSArray as? [Int])!;
+            
+            self.performSegue(withIdentifier: "enterGame", sender: [otherPlayer, algorithm, arr]);
+        }
+    }
 
+    @IBAction func onSearchPress(_ sender: Any) {
+        self.socket.emit("enter_game_queue", Auth.auth().currentUser!.email!, Auth.auth().currentUser!.uid);
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if (segue.identifier == "enterGame") {
+//            let vc = segue.destination as! GameViewController;
+//            let data = sender as! NSArray;
+//            vc.otherPlayerName = (data[0] as! String);
+//            vc.algorithm = (data[1] as! String);
+//            vc.arr = data[2] as! [Int];
+//            vc.socket = self.socket;
+//        }
+    }
 }
